ac13f3460eeaf74b38621a90667b581a7656d1e7
sanket-0904 <sablesanket34@gmail.com>
2025-03-31 12:12:08 +0530

    today

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
-// import Cards from "./components/Cards/Cards";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
-      {/* <Cards /> */}
     </div>
   );
 };
