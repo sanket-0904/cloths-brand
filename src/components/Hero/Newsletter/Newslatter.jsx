15e34b52a6d28cb0eb92e5f97b6496bac951c315
sanket-0904 <sablesanket34@gmail.com>
2025-04-03 18:13:38 +0530

    Add newslatter

---
+++ src/components/Hero/Newsletter/Newslatter.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./Newslatter.module.css";
+
+const Newslatter = () => {
+  return (
+    <div className={styles.main}>
+      <div className={styles.content}>
+        <h4>Sign Up For Newsletters</h4>
+        <p>
+          Get E-mail update about our letest shop <span>special offer.</span>
+        </p>
+      </div>
+      <div className={styles.email}>
+        <input type="text" name="email" id="email" />
+        <button>Sign up</button>
+      </div>
+    </div>
+  );
+};
+
+export default Newslatter;
