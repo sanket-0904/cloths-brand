d808d84adc20345b5e0ff471e6f9b27a74c485b4
sanket-0904 <sablesanket34@gmail.com>
2025-04-06 00:35:54 +0530

    Recover

---
+++ src/components/Hero/Hero/Hero.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import style from "./Hero.module.css";
+import "@fontsource/spartan"; // ✅ Direct import
+import "@fontsource/poppins/500.css"; // popppins font
+import Cards from "../../Cards/Cards.jsx";
+import Products from "../../Products/Products.jsx";
+import Banners from "../../Banner/Banner.jsx";
+import Productstwo from "../../Productstwo/Productstwo.jsx";
+import Smbanners from "../Smbanner/Smbanner.jsx";
+import Bannerthrees from "../Bannerthree/Bannerthree.jsx";
+import Newslatter from "../Newsletter/Newslatter.jsx";
+import Futter from "../../Footer/Footer.jsx";
+
+const Hero = () => {
+  return (
+    <div>
+      <div className={style.hero}>
+        <div className={style.content}>
+          <h4>Trade-in-offer</h4>
+          <h2>Super value deals</h2>
+          <h1>On all products</h1>
+          <p>Save more with coupons & up to 70% off!</p>
+          <button className={style.herobtn}>Shop Now</button>
+        </div>
+      </div>
+      <Cards />
+      <div className={style.title}>
+        <h1>Featured Products</h1>
+        <p>Summer Collection New Morden Design</p>
+      </div>
+      <Products />
+      <Banners />
+      <div className={style.title}>
+        <h1>New Arrivals</h1>
+        <p>Summer Collection New Morden Design</p>
+      </div>
+      <Productstwo />
+      <Smbanners />
+      <Bannerthrees />
+      <Newslatter />
+      <Futter />
+    </div>
+  );
+};
+
+export default Hero;
