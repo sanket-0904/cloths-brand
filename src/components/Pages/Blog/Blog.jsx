e1db803c66af132ec04da5eb91b899ea897294e7
sanket-0904 <sablesanket34@gmail.com>
2025-05-31 15:16:59 +0530

    add bannner in blog

--- src/components/Pages/Blog/Blog.jsx
+++ src/components/Pages/Blog/Blog.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import styles from "./../Blog/blog.module.css";
 
 const Blog = () => {
-  return <div>hello blog</div>;
+  return (
+    <>
+      <div className={styles.main}>
+        <h1>#readmore</h1>
+        <h5>Read all case studies about our products!</h5>
+      </div>
+    </>
+  );
 };
 
 export default Blog;
